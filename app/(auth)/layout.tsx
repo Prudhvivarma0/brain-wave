6c2e2cf59e570405a66739381dd7c14c82179265
AryanRajeshK <aryankrk007@gmail.com>
2023-12-26 16:08:47 +0530

    login and theme

---
+++ app/(auth)/layout.tsx
@@ -0,0 +1,13 @@
+const AuthLayout = ({ children }: { children: React.ReactNode }) => {
+    return (
+      <div
+        className="h-full flex items-center justify-center"
+        style={{ backgroundColor: '#A020F0' }}
+      >
+        {children}
+      </div>
+    );
+  };
+  
+  export default AuthLayout;
+  

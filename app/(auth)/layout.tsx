d68716100a4819394cf07d4b3d1b097412db41e0
AryanRajeshK <aryankrk007@gmail.com>
2024-01-05 21:05:52 +0400

    Login page bg

--- app/(auth)/layout.tsx
+++ app/(auth)/layout.tsx
@@ -1,12 +1,24 @@
+import React from 'react';
+
 const AuthLayout = ({ children }: { children: React.ReactNode }) => {
-    return (
-      <div
-        className="h-full flex items-center justify-center"
-        style={{ backgroundColor: '#A020F0' }}
-      >
-        {children}
-      </div>
-    );
-  };
   
-  export default AuthLayout;
+  const backgroundImage = 'url("/login.png")'; 
+
+  return (
+      <div
+        className='items-end'
+        style={{
+          height: '100vh',
+          display: 'flex',
+          alignItems: 'center',
+          justifyContent: 'end',
+          backgroundImage: backgroundImage,
+          backgroundSize: 'cover',
+          paddingRight: '50px'
+        }}>
+        {children}
+      </div> 
+  );
+};
+
+export default AuthLayout;

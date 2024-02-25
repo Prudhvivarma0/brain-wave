a7ac58e4427cd3e2d3f0de86279d38b928f81a23
Ibrahim <ibrahimrathwala@gmail.com>
2024-02-25 17:41:28 +0400

    challenges

--- app/(challenges)/challenges/page.tsx
+++ app/(challenges)/challenges/page.tsx
@@ -9,9 +9,9 @@ import { redirect } from "next/navigation";
 
 const Challenges = async ({
     children
-}: {children: React.ReactNode}) => {
-    const challenge = await db.challenge.findFirst({
-
+}: { children: React.ReactNode }) => {
+    const server = await db.server.findMany({
+        select: { id: true }
     });
     const currprofile = await currentProfile();
     if (!currprofile) {
@@ -19,18 +19,18 @@ const Challenges = async ({
     }
     const challenges = await db.challenge.findMany({
     });
-    
-    return ( 
+
+    return (
         <div className="h-full">
             <div className="hidden md:flex h-full w-[155px] z-30 flex-col fixed inset-y-0">
-                <NavigationSidebar/>
+                <NavigationSidebar />
             </div>
             <main className="md:pl-[180px] pr-[20px] h-full">
-                <HomeMobileToggle/>
+                <HomeMobileToggle />
                 <div className="flex items-center justify-between mt-5">
-                        <div className="text-4xl ml-9 mb-5" >
+                    <div className="text-4xl ml-9 mb-5" >
                         <strong>CHALLENGES</strong>
-                        </div>
+                    </div>
                 </div>
                 <Separator className="h-[3px] dark:bg-[rgb(92,41,96)] bg-[rgb(56,37,91)] w-full mt-2" />
                 <div className="flex justify-between">
@@ -38,24 +38,25 @@ const Challenges = async ({
                     <div className="text-4xl font-bold text-center py-4 ml-[270px]">Prize</div>
                     <div className="text-4xl font-bold text-center py-4  mr-12">Duration</div>
                 </div>
-                <div className="flex items-center flex-wrap gap-3 p-9">
-                {challenges.map((challenge) => (
+                <div className="flex items-center flex-wrap gap-3 p-9"> 
+                    {challenges.filter(challenge => server.some(server => server.id === challenge.serverId))
+                        .map((challenge) => (
                             <div key={challenge.id} className="mb-1 w-full">
                                 <NavigationItem
                                     id={challenge.id}
                                     name={challenge.name}
                                     prize={challenge.prize}
-                                    duration = {challenge.duration}                                                                    
+                                    duration={challenge.duration}
                                 />
                             </div>
                         ))}
                 </div>
                 <div className="flex items-center">
-                            <NavigationAction />
-                </div>               
+                    <NavigationAction />
+                </div>
             </main>
         </div>
-     );
+    );
 }
- 
+
 export default Challenges;

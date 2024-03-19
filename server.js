346879f2a705290751548ab2c4580363ac01b5f1
Ibrahim <ibrahimrathwala@gmail.com>
2024-03-19 04:54:35 +0400

    DB Fixed

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose")
 const Document = require("./Document");
 const { data } = require("autoprefixer");
 
-mongoose.connect("mongodb://localhost:27017/google-docs-clone", {
+mongoose.connect("mongodb+srv://yoboz:tiger@yoboz.e4xsvsi.mongodb.net/google-docs-clone", {
   useNewUrlParser: true,
   useUnifiedTopology: true,
 });

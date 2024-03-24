776074a65edcf6ceafebdd6bd019d66d1b86cae2
Prudhvi Rudra Raju <114640816+Prudhvivarma0@users.noreply.github.com>
2024-03-24 22:54:06 +0400

    ui change

--- components/navigation/navigation-action2.tsx
+++ components/navigation/navigation-action2.tsx
@@ -3,17 +3,19 @@
 import { useModal } from "@/hooks/use-modal-store";
 
 export const NavigationAction = () => {
-    const {onOpen} = useModal();
-    return(
-        <div style={{position: 'fixed', bottom: '20px', right: '20px' }}>
+    const { onOpen } = useModal();
+
+    return (
+        <div style={{ position: 'fixed', top: '170px', left: '200px' }}>
             <button
-            onClick={() => onOpen("createChallenge")}
-            className="group items-center"
+                onClick={() => onOpen("createChallenge")}
+                className="group items-center"
+                style={{ position: 'fixed' }} // Set position to 'fixed'
             >
-                <div className="flex h-[40px] w-[150px] rounded-[12px]  items-center justify-center bg-[rgb(102,26,138)] group-hover:bg-[rgb(122,46,158)]">
-                    <div className="text-white">Create Challenge</div>
+                <div className="h-[40px] w-[150px] rounded-[12px] items-center justify-center bg-[rgb(102,26,138)] group-hover:bg-[rgb(122,46,158)]">
+                    <div className="text-white">Host a Challenge</div>
                 </div>
             </button>
         </div>
-    )
-}
+    );
+};

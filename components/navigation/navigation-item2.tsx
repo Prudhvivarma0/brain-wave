2c65d28e22886655b4a7a00c6a96dcdceee1736b
Prudhvi <prudhvivarma31@gmail.com>
2024-03-25 17:27:37 +0400

    challenge cards ui

--- components/navigation/navigation-item2.tsx
+++ components/navigation/navigation-item2.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import { useParams, useRouter } from "next/navigation";
-import { Trophy, Clock } from 'lucide-react'; // Importing icons for prize and duration
+import { Trophy, Clock } from 'lucide-react';
 import React from "react";
 
 interface NavigationItemProps {
@@ -29,26 +29,27 @@ export const NavigationItem = ({
     };
 
     return (
-        <div onClick={handleClick} className="bg-[rgb(69,38,93)] group-hover:bg-[rgb(89,58,113)] rounded-[10px] group-hover:rounded-[20px] border border-gray-300 mt-9 mb-4 h-[450px] flex flex-col"> {/* Add onClick to the container */}
+        <div onClick={handleClick} className="bg-[rgb(69,38,93)] group-hover:bg-[rgb(89,58,113)] rounded-[10px] group-hover:rounded-[20px] border border-gray-300 mt-9 mb-4 h-[450px] flex flex-col">
             <button onClick={handleClick} className="flex-1">
                 <div className="image-container w-[180px] h-[180px] rounded-[10px] group-hover:rounded-[20px] border border-gray-200 mb-2 mx-auto">
                     <img src={img} alt="challenge" className="w-full h-full rounded-[10px]" />
                 </div>
-                <div className="text-white text-center">{name}</div> {/* Displaying name below the picture */}
+                <div className="text-white text-center">{name}</div>
             </button>
             <div className="pl-[20px] pb-[20px]">
-                <div className="text-white mb-[10px] h-[110px] overflow-hidden">Objective: {by}</div> {/* Set a fixed height for the div containing the objective */}
+                <div className="text-white mb-[10px] h-[110px] overflow-hidden">Objective: {by}</div>
                 <div className="text-white flex justify-center items-center">
-                    <div className="flex items-center mr-2">
-                        <Trophy size={20} /> {/* Displaying trophy icon */}
-                        <span>{prize}</span> {/* Displaying prize */}
+                    <div className="flex flex-col items-center mr-12">
+                        <Trophy size={24} />
+                        <span className="mt-[5px] text-lg">{prize}</span>
                     </div>
-                    <div className="flex items-center">
-                        <Clock size={20} /> {/* Displaying clock icon */}
-                        <span>{duration.split(' ').map(word => word.charAt(0).toUpperCase() + word.slice(1)).join(' ')}</span> {/* Displaying capitalized duration */}
+                    <div className="flex flex-col items-center">
+                        <Clock size={24} />
+                        <span className="mt-[5px] text-lg">{duration.split(' ').map(word => word.charAt(0).toUpperCase() + word.slice(1)).join(' ')}</span>
                     </div>
                 </div>
             </div>
+
         </div>
     );
 };

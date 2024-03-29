65e9112bfc591289d243bf484d96bc723851347b
Ibrahim <ibrahimrathwala@gmail.com>
2024-03-29 12:34:45 +0400

    fix

--- middleware.ts
+++ middleware.ts
@@ -4,11 +4,7 @@ import { authMiddleware } from "@clerk/nextjs";
 // Please edit this to allow other routes to be public as needed.
 // See https://clerk.com/docs/references/nextjs/auth-middleware for more information about configuring your Middleware
 export default authMiddleware({
-<<<<<<< HEAD
   publicRoutes: ["/api/uploadthing","/pagelanding"]
-=======
-  publicRoutes: ["/api/uploadthing", "/pagelanding"]
->>>>>>> b597985edb9b6c98bfeceebb55a9a7a2cf8e97af
 });
  
 export const config = {
